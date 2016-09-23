0d4a56f5f5d430c965edcfc610364c5eae6e6939
b5410545991 <gent_playbox@hotmail.com>
2016-09-23 15:48:06 +0700

    aaa

---
+++ desktop/src/com/mygdx/game/pacmann/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.pacmann.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.pacmann.Pacmannnnnn;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new Pacmannnnnn(), config);
+	}
+}
